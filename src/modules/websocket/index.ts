b3807dfc901e0e1fc66e928b01541f0d6415e525
BrightSkyz <BrightSkyz@users.noreply.github.com>
2020-07-03 11:35:31 -0400

    Blacklist some IPs from being used for checks

--- src/modules/websocket/index.ts
+++ src/modules/websocket/index.ts
@@ -31,9 +31,7 @@ export class WebSocketModule extends Module {
             // Process request
             const type = String(jsonMessage.type);
             const data = String(jsonMessage.data);
-            const isValidAddress4 = new ipAddress.Address4(data).isValid();
-            const isValidAddress6 = new ipAddress.Address6(data).isValid();
-            if (isValidAddress4 || isValidAddress6) {
+            if (this.isValidV4(data) || this.isValidV6(data)) {
               if (type == "ping") {
                 this.runCommandAndSendOutput(ws, "ping", ["-c", "8", "-i", "0.2", "-t", "255", data]);
               } else if (type == "traceroute") {
@@ -85,7 +83,7 @@ export class WebSocketModule extends Module {
     return this.webSocketServer;
   }
 
-  runCommandAndSendOutput(ws: websocket, command: string, args: Array<string>) {
+  runCommandAndSendOutput(ws: websocket, command: string, args: Array<string>): void {
     ws.send(JSON.stringify({
       type: "status",
       data: "start"
@@ -117,6 +115,42 @@ export class WebSocketModule extends Module {
         data: "end"
       }));
     });
+  }
+
+  isValidV4(address: string): boolean {
+    const address4 = new ipAddress.Address4(address);
+    if (!address4.isValid()) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("0.0.0.0/8"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("10.0.0.0/8"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("100.64.0.0/10"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("127.0.0.0/8"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("169.254.0.0/16"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("172.16.0.0/12"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("192.0.0.0/24"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("192.0.2.0/24"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("192.168.0.0/16"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("198.18.0.0/15"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("198.51.100.0/24"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("203.0.113.0/24"))) return false;
+    if (address4.isInSubnet(new ipAddress.Address4("224.0.0.0/3"))) return false;
+    return true;
+  }
+
+  isValidV6(address: string): boolean {
+    const address6 = new ipAddress.Address6(address);
+    if (!address6.isValid()) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("::/128"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("::1/128"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("::ffff/96"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("fc00::/7"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("fe80::/10"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("2001:0000::/32"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("2001:0002::/48"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("2001:0010::/28"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("2002::/16"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("2001:db8::/32"))) return false;
+    if (address6.isInSubnet(new ipAddress.Address6("ff00::/8"))) return false;
+    return true;
   }
 
 }
